7d7d05465e67aa4819a6378b0c5b00488d8979be
Chavakan Yimmark <chavakan.yim@student.mahidol.ac.th>
2021-02-26 07:54:22 +0000

    push and pop

--- stack.h
+++ stack.h
@@ -3,12 +3,33 @@
 #define stack_h
 #include "node.h"
 typedef struct {
-	NodePtr top;
-	int size;
+	NodePtr top = NULL;
+	int size = 0;
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(Stack s);
-void pop_all(Stack s);
+
+void push(StackPtr s, char value){
+  NodePtr now = (NodePtr)malloc(sizeof(NodePtr));
+  if(now){
+    now->data = value;
+    now->nextPtr = s->top;
+    s->size += 1;
+    s->top = now;
+  }
+}
+
+char pop(StackPtr s){
+  char val;
+  NodePtr n = s->top;
+  if(s->top!=NULL){
+    val = n->data;
+    s->top = n->nextPtr;
+    free(n);
+    s->size -= 1;
+    return val;
+  }
+  else return NULL;
+}
+void pop_all(StackPtr s);
 #endif
